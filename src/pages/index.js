0ecaa4869237c8e14607df7878d5c28a914ff89d
ekinomicss <zorerekin@gmail.com>
2024-09-02 00:07:55 -0400

    md to html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { getSortedPostsData } from '../utils/markdownToHtml';
 
-const Home = () => {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+const Home = ({ allPostsData }) => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the home page of the blog.</p>
+      <ul>
+        {allPostsData.map(({ id, title, date }) => (
+          <li key={id}>
+            <a href={`/posts/${id}`}>{title}</a>
+            <br />
+            <small>{date}</small>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default Home;np
+export default Home;
