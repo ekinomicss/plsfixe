185e31e8cf2ae51c9ddaac56d46f4574e3323915
ekinomicss <zorerekin@gmail.com>
2024-09-05 19:02:30 -0400

    thumbnail now directs to slug

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 const PostCard = ({ post }) => {
   return (
     <div className="border border-black rounded-lg p-4 hover:shadow-lg transition-shadow duration-300">
+      <Link href={`/posts/${post.slug}`}>
       {post.thumbnail && (
         <img
           src={post.thumbnail}
@@ -11,6 +12,7 @@ const PostCard = ({ post }) => {
           className="w-full h-48 object-cover rounded-t-lg mb-4"
         />
       )}
+      </Link>
       <h2 className="text-xl font-semibold font-sans mb-2">
         <Link href={`/posts/${post.slug}`} className="text-yellow-600">
           {post.title}
