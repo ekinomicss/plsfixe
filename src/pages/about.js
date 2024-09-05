ef21384c7c18f88340599b618f2155fb593cbe4f
ekinomicss <zorerekin@gmail.com>
2024-09-05 11:51:10 -0400

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,16 +3,20 @@ import Layout from '../components/Layout';
 const About = () => {
   return (
     <Layout>
-      <div className="container mx-auto py-12 px-6">
+      <div className="container mx-auto py-12 font-sans px-6">
         <h1 className="text-4xl font-bold font-serif mb-6">About Us</h1>
+        <img src = "/images/plsfixe_logo_no_bg.png" className = "h-40 w-40 mb-5"></img>
         <p className="text-lg mb-4">
-          Welcome to PLS FIXE NYC! We are a passionate team of food lovers dedicated to exploring the diverse culinary landscape of New York City.
+          Welcome to PLS FIXE NYC! You might recognize us from our instagram, <a href = "https://instagram.com/plsfixenyc">@plsfixenyc.</a>
         </p>
         <p className="text-lg mb-4">
-          Our blog showcases the best local spots, hidden gems, and food experiences across the city. We believe in celebrating the unique flavors and stories behind every dish.
+          We wanted to start a blog to answer some of our friend's most existential questions: "Where should I dine tonight?", "What should I order?" and "What's the vibe??"
         </p>
-        <p className="text-lg">
-          Whether you're looking for food recommendations or curious about the latest trends, PLS FIXE NYC is here to guide you. Follow along as we taste our way through NYC’s vibrant food scene.
+        <p className="text-lg mb-4">
+          As a group of young professionals in the city, we are grateful to be able to have access to some of the best food in the world. We hope to share our experiences with you and help you make the most of your dining experiences in NYC!
+        </p>
+        <p className="text-lg italic">
+        For partnerships and promos, <a href = "/contact">contact us.</a>
         </p>
       </div>
     </Layout>
