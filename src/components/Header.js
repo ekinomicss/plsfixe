3c48e4ce824cd345f24133a14bc9200e8a064c03
ekinomicss <zorerekin@gmail.com>
2024-09-05 14:50:55 -0400

    new navbar and about fmtting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 // src/components/Header.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 export default function Header() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  // const timeoutRef = useRef(null);
+
   return (
     <header className="fixed w-full bg-[#ede6d8] border-b-8 border-double border-black py-4 z-50">
       <div className="container mx-auto flex justify-between items-center px-4">
@@ -26,12 +29,31 @@ export default function Header() {
           <Link href="/about" className="text-black hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif">
             ABOUT
           </Link>
-          <Link href="/posts" className="text-black hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif">
-            REVIEWS
-          </Link>
-          <Link href="/map" className="text-black hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif">
-            MAP
-          </Link>
+          
+          {/* Dropdown for DINE */}
+          <div className="relative" onMouseEnter={() => setDropdownOpen(true)} onMouseLeave={() => setDropdownOpen(false)}>
+          <button className="text-black hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif bg-transparent focus:outline-none"
+            style={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+            DINE
+          </button>
+            {dropdownOpen && (
+              <div className="absolute mt-0 bg-[#ede6d8] border border-black rounded-lg shadow-lg">
+                <ul className="py-2">
+                  <li>
+                    <Link href="/posts" className="block px-4 py-2 font-bold font-serif text-black hover:bg-yellow-600 hover:text-white">
+                      POSTS
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/map" className="block px-4 py-2 font-bold font-serif text-black hover:bg-yellow-600 hover:text-white">
+                      MAP
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
           <Link href="/contact" className="text-black hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif">
             CONTACT
           </Link>
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,16 +6,16 @@ const About = () => {
       <div className="container mx-auto py-12 font-sans px-6">
         <h1 className="text-4xl font-bold font-serif mb-6">About Us</h1>
         <img src = "/images/plsfixe_logo_no_bg.png" className = "h-40 w-40 mb-5"></img>
-        <p className="text-lg mb-4">
+        <p className="text-md mb-4">
           Welcome to PLS FIXE NYC! You might recognize us from our instagram, <a href = "https://instagram.com/plsfixenyc">@plsfixenyc.</a>
         </p>
-        <p className="text-lg mb-4">
+        <p className="text-md mb-4">
           We wanted to start a blog to answer some of our friends' most existential questions: "Where should I dine tonight?", "What should I order?" and "What's the vibe??"
         </p>
-        <p className="text-lg mb-4">
+        <p className="text-md mb-4">
           As a group of young professionals in the city, we are grateful to be able to have access to some of the best food in the world. We hope to share our experiences with you and help you make the most of your dining experiences in NYC!
         </p>
-        <p className="text-lg italic">
+        <p className="text-md italic">
         For partnerships and promos, <a href = "/contact">contact us.</a>
         </p>
       </div>
