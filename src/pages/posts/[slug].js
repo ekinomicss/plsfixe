928fcb53d63ae3b8f44c4fb1cb9c90591a9b84b4
ekinomicss <zorerekin@gmail.com>
2024-09-08 10:01:29 -0400

    move date and cat under thumbnail

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -25,13 +25,14 @@ const Post = ({ postData }) => {
     <Layout>
       <article className="container mx-auto py-12 font-sans px-6">
         <h1 className="text-4xl font-bold font-serif mb-6">{postData.title}</h1>
-        <p className="text-md mb-4 font-serif">
+        
+          <img src = {postData.thumbnail} className = " w-full h-64 object-cover mb-3 rounded-lg"></img>
+          <p className="text-md mb-8 font-serif">
           {postData.category && ` ${postData.category}`}
           {postData.neighborhood && ` | ${postData.neighborhood}`}
           <br></br>
           <i>Last updated: {postData.date} </i>
           </p>
-          <img src = {postData.thumbnail} className = " w-full h-64 object-cover mb-6 rounded-lg"></img>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         {/* Go to all posts page */}
         <a href="/posts"><h4 className="text-lg font-bold underline font-serif mt-5 hover:text-yellow-600 duration-300 ease-in-out">&lt; All Posts</h4></a>
