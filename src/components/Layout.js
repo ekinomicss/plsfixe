fc3a15b66048653049d2ab5ca60e45b6a9a0d8fd
ekinomicss <zorerekin@gmail.com>
2024-09-11 19:24:29 -0400

    adding page transition fade in/out

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,25 @@
-// src/components/Layout.js
-
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }) {
+  const router = useRouter();
+
   return (
     <>
       <Header />
       <main className="container mx-auto px-20 py-20">
-        {children}
+      <motion.div
+      key={router.pathname}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      {children}
+    </motion.div>
       </main>
       <Footer />
     </>
