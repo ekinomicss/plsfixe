5c04f046a97aa696eccbb5e72094adfa8ff86097
ekinomicss <zorerekin@gmail.com>
2024-09-12 10:56:37 -0400

    double border container for slug text

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -26,17 +26,17 @@ const Post = ({ postData }) => {
       <article className="container mx-auto py-12 font-sans px-6">
         <h1 className="text-4xl font-bold font-serif mb-6">{postData.title}</h1>
           <img src = {postData.thumbnail} className = " w-full h-96 object-cover mb-3 rounded-lg border-4 border-double border-black"></img>
-          <p className="text-md mb-4 font-serif">
+          <p className="text-md mb-- font-serif">
             {postData.category && ` ${postData.category}`}
             {postData.neighborhood && ` | ${postData.neighborhood}`}
             <br></br>
             <i>Last updated: {postData.date} </i>
           </p>
-      <div className="flex flex-col mb-10">
+      <div className="flex flex-col mb-2">
         <img src = "/images/divider1.png" className="w-80 mx-auto"></img>
       </div>
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      <div className="flex flex-col mt-10">
+          <div className="border-4 border-double border-black rounded-lg p-6" dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <div className="flex flex-col mt-2">
         <img src = "/images/divider1_bottom.png" className="w-80 mx-auto"></img>
       </div>
         {/* Go to all posts page */}
