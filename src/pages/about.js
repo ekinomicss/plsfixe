cd823406d4fbb98c036f2d3ee3fcfe2075bc0082
ekinomicss <zorerekin@gmail.com>
2024-09-11 22:09:56 -0400

    somewhat better about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,22 +4,28 @@ const About = () => {
   return (
     <Layout>
       <div className="container mx-auto py-12 font-sans px-6">
-        <h1 className="text-4xl font-bold font-serif mb-6">About Us</h1>
-        <img src = "/images/plsfixe_logo_no_bg.png" className = "h-40 w-40 mb-5"></img>
-        <p className="text-md mb-4">
+        <h1 className="text-4xl font-bold font-serif mb-6 hover:text-yellow-500 duration-300 ease-in-out ">About</h1>
+        <img src = "/images/plsfixe_logo_no_bg.png" className = "h-40 w-40 mb-5 block mx-auto"></img>
+      <div className="flex flex-col space-y-0 mb-7">
+        <img src = "/images/divider1.png" className="w-80 mx-auto"></img>
+      </div>
+        <p className="text-lg mb-4 text-center">
           Welcome to PLS FIXE NYC! You might recognize us from our instagram, <a href = "https://instagram.com/plsfixenyc">@plsfixenyc.</a>
         </p>
-        <p className="text-md mb-4">
+        <p className="text-md mb-4 text-center">
           We wanted to start a blog to answer some of our friends' most existential questions: "Where should I dine tonight?", "What should I order?" and "What's the vibe??"
         </p>
-        <p className="text-md mb-4">
+        <p className="text-md mb-4 text-center">
           Due to our small size, we are only covering Manhatan and Brooklyn. We hoping to extend to other boroughs as we grow.        </p>
-        <p className="text-md mb-4">
+        <p className="text-md mb-4 text-center">
           As a group of young professionals in the city, we are grateful to be able to have access to some of the best food in the world. We hope to share our experiences with you and help you make the most of your dining experiences in NYC!
         </p>
-        <p className="text-md italic">
+        <p className="text-md italic text-center">
         For partnerships and promos, <a href = "/contact">contact us.</a>
         </p>
+        <div className="flex flex-col space-y-0 mt-6">
+        <img src = "/images/divider1_bottom.png" className="w-80 mx-auto"></img>
+      </div>
       </div>
     </Layout>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      textStrokeWidth: {
+        '1': '1px',
+        '2': '2px',
+      },
+      textStrokeColor: {
+        black: '#000',
+        white: '#fff',
+      },
       fontFamily: {
         bodoni: ['Bodoni Moda SC', 'serif'],
       },
@@ -12,6 +20,18 @@ module.exports = {
       const newUtilities = {
         '.small-caps': {
           'font-variant': 'small-caps',
+        },
+        '.text-stroke': {
+          '-webkit-text-stroke': '1px',
+        },
+        '.text-stroke-2': {
+          '-webkit-text-stroke': '2px',
+        },
+        '.text-stroke-black': {
+          '-webkit-text-stroke-color': '#000',
+        },
+        '.text-stroke-white': {
+          '-webkit-text-stroke-color': '#fff',
         },
       };
       addUtilities(newUtilities);
