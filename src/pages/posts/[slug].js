1def383e44c0c1392a2b13fc47bffc1f0ea569ba
ekinomicss <zorerekin@gmail.com>
2024-09-11 22:30:01 -0400

    add border and divider to slug

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -25,15 +25,20 @@ const Post = ({ postData }) => {
     <Layout>
       <article className="container mx-auto py-12 font-sans px-6">
         <h1 className="text-4xl font-bold font-serif mb-6">{postData.title}</h1>
-        
-          <img src = {postData.thumbnail} className = " w-full h-96 object-cover mb-3 rounded-lg"></img>
-          <p className="text-md mb-8 font-serif">
-          {postData.category && ` ${postData.category}`}
-          {postData.neighborhood && ` | ${postData.neighborhood}`}
-          <br></br>
-          <i>Last updated: {postData.date} </i>
+          <img src = {postData.thumbnail} className = " w-full h-96 object-cover mb-3 rounded-lg border-4 border-double border-black"></img>
+          <p className="text-md mb-4 font-serif">
+            {postData.category && ` ${postData.category}`}
+            {postData.neighborhood && ` | ${postData.neighborhood}`}
+            <br></br>
+            <i>Last updated: {postData.date} </i>
           </p>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <div className="flex flex-col mb-10">
+        <img src = "/images/divider1.png" className="w-80 mx-auto"></img>
+      </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <div className="flex flex-col mt-10">
+        <img src = "/images/divider1_bottom.png" className="w-80 mx-auto"></img>
+      </div>
         {/* Go to all posts page */}
         <a href="/posts"><h4 className="text-lg font-bold underline font-serif mt-5 hover:text-yellow-600 duration-300 ease-in-out">&lt; All Posts</h4></a>
       </article>
