30eaa22de8d1e209019c2b272d11ca5d84a6e338
ekinomicss <zorerekin@gmail.com>
2024-09-13 23:25:34 -0400

    add mr fetch package

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,23 +3,33 @@ import Header from './Header';
 import Footer from './Footer';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
+import Search from '../utils/Search'; // Import the Search component to check for it
 
 export default function Layout({ children }) {
   const router = useRouter();
+
+  // Check if `children` is the `Search` component
+  const isSearchComponent = React.Children.toArray(children).some(
+    (child) => child.type === Search
+  );
 
   return (
     <>
       <Header />
       <main className="container mx-auto px-20 py-20">
-      <motion.div
-      key={router.pathname}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-    >
-      {children}
-    </motion.div>
+        {!isSearchComponent ? (
+          <motion.div
+            key={router.pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            {children}
+          </motion.div>
+        ) : (
+          children // Render `Search` without `motion.div`
+        )}
       </main>
       <Footer />
     </>
