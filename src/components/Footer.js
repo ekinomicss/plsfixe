1923cfe99f6d44c1b1dbd6e9028e1cae45eafa3d
ekinomicss <zorerekin@gmail.com>
2024-09-14 18:20:12 -0400

    eh i like it better black

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,27 +5,27 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="bg-[#f5d27d] text-gray py-6 border-b-8 border-t-8 border-double border-black">
+    <footer className="bg-black text-gray py-6 border-b-8 border-t-8 border-double border-white">
       <div className="container mx-auto px-4 text-center">
         <p className="mb-4 ">© 2024 Pls Fixe Nyc.</p>
-        <ul className="flex justify-center space-x-4 color-beige">
+        <ul className="flex text-white justify-center space-x-4 color-beige">
           <li>
-            <Link href="/" className="text-yellow-800 hover:text-yellow-600">
+            <Link href="/" className=" text-gray-300 hover:text-yellow-600">
               Home
             </Link>
           </li>
           <li>
-            <Link href="/about" className=" text-yellow-800 hover:text-yellow-600">
+            <Link href="/about" className=" text-gray-300 hover:text-yellow-600">
               About
             </Link>
           </li>
           <li>
-            <Link href="/posts" className="text-yellow-800 hover:text-yellow-600">
+            <Link href="/posts" className="text-gray-300 hover:text-yellow-600">
               Reviews
             </Link>
           </li>
           <li>
-            <Link href="/contact" className="text-yellow-800 hover:text-yellow-600">
+            <Link href="/contact" className="text-gray-300 hover:text-yellow-600">
               Contact
             </Link>
           </li>
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
-    <header className="fixed w-full bg-[#f5d27d] border-b-8 border-double border-black py-4 z-50">
+    <header className="fixed w-full bg-black border-b-8 border-double border-white py-4 z-50">
       <div className="container mx-auto flex justify-between items-center px-4 flex-wrap md:flex-nowrap">
         {/* Logo and Title on the Left */}
         <div className="flex items-center space-x-4">
@@ -14,7 +14,7 @@ export default function Header() {
             <img src="/images/utensils_nobg.png" alt="Logo" className="h-10 w-10 sm:h-14 sm:w-14" /> {/* Adjust image size for smaller screens */}
           </Link>
           <a href="/" className="block">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl text-black font-serif font-bold italic hover:text-yellow-600 duration-300 ease-in-out [text-decoration:underline_overline]">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl text-white font-serif font-bold italic hover:text-yellow-600 duration-300 ease-in-out [text-decoration:underline_overline]">
               <span className="font-bold italic [font-variant:small-caps]">
                 Pls Fixe Nyc
               </span>
@@ -33,21 +33,21 @@ export default function Header() {
             onMouseLeave={() => setDropdownOpen(false)}
           >
             <button
-              className="text-black hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif bg-transparent focus:outline-none"
+              className="text-white hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif bg-transparent focus:outline-none"
               style={{ backgroundColor: 'transparent', boxShadow: 'none' }}
             >
               DINE
             </button>
             {dropdownOpen && (
-              <div className="absolute mt-0 bg-[#ede6d8] border border-black rounded-lg shadow-lg transform transition-all duration-300 ease-in-out">
+              <div className="absolute mt-0 bg-yellow-800 border border-black rounded-lg shadow-lg transform transition-all duration-300 ease-in-out">
                 <ul className="py-2">
                   <li>
-                    <Link href="/posts" className="block px-4 py-2 font-bold font-serif text-black hover:bg-yellow-600 hover:text-white">
+                    <Link href="/posts" className="block px-4 py-2 font-bold font-serif text-white hover:bg-yellow-600 hover:text-white">
                       POSTS
                     </Link>
                   </li>
                   <li>
-                    <Link href="/map" className="block px-4 py-2 font-bold font-serif text-black hover:bg-yellow-600 hover:text-white">
+                    <Link href="/map" className="block px-4 py-2 font-bold font-serif text-white hover:bg-yellow-600 hover:text-white">
                       MAP
                     </Link>
                   </li>
@@ -56,7 +56,7 @@ export default function Header() {
             )}
           </div>
 
-          <Link href="/contact" className="text-black hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif">
+          <Link href="/contact" className="text-white hover:text-yellow-600 hover:underline duration-300 ease-in-out font-bold font-serif">
             CONTACT
           </Link>
         </nav>
