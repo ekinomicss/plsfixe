a44801b062e2c80b2ce071420740e7f8823df408
ekinomicss <zorerekin@gmail.com>
2024-09-13 20:54:54 -0400

    update gitignore

--- src/utils/Hit.jsx
+++ src/utils/Hit.jsx
@@ -8,7 +8,7 @@ const Hit = ({ hit }) => {
         href={`/posts/${hit.objectID}`}
         className="hit-link block py-0 hover:bg-gray-300"
       >
-        <h2 className="text-md font-bold mb-0 font-medium hover:text-yellow-600">{hit.title}</h2>
+        <h2 className="text-md font-bold mb-0 font-serif hover:text-yellow-600">{hit.title}</h2>
         <p className="text-sm text-gray-600">
           {hit.category} || {hit.neighborhood}
         </p>
