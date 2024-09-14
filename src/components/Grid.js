bf7461fc63d12e37bf52726293c5ded6b8a35e91
ekinomicss <zorerekin@gmail.com>
2024-09-13 23:53:03 -0400

    mobile compatilibity /posts

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,7 +3,7 @@ import PostCard from './PostCard';
 
 const Grid = ({ posts }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 py-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 py-4 px-2 sm:px-0">
       {posts.map((post) => (
         <PostCard key={post.slug} post={post} />
       ))}
