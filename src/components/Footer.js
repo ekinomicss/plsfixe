fa4a3bbb3468aa234a49df7aef42f946bb79458f
ekinomicss <zorerekin@gmail.com>
2024-10-26 14:06:31 +0300

    fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="bg-black text-gray py-6 border-b-8 border-t-8 border-double border-white">
-      <div className="container mx-auto px-4 text-center">
-        <p className="mb-4 ">© 2024 Pls Fixe Nyc.</p>
-        <ul className="flex text-white justify-center space-x-4 color-beige">
+      <div className="container mx-auto text-center">
+        <p className="mb-4 indent-0 ">© 2024 Pls Fixe Nyc.</p>
+        <ul className="flex text-white justify-center space-x-4 color-beige list-none m-0">
           <li>
             <Link href="/" className=" text-gray-300 hover:text-yellow-600">
               Home
