fabd52669cdba91eaf7b45beb90092af6ade40c2
ekinomicss <zorerekin@gmail.com>
2024-10-28 23:45:42 +0300

    update contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,8 +5,8 @@ const About = () => {
     <Layout>
       <div className="container mx-auto py-12 px-6">
         <h1 className="text-3xl font-bold font-serif mb-6">Contact Us</h1>
-        <p className="text-lg mb-4">
-        plsfixenyc@gmail.com
+        <p className="text-lg mb-4 indent-0">
+          For partnerships and other inquiries, please email <i>plsfixenyc@gmail.com.</i>
         </p>
         
       </div>
