af03fbe232eb5ecfa34d775350d3b4ee3d8808a8
ekinomicss <zorerekin@gmail.com>
2024-11-02 15:15:07 -0400

    hitbox more compact

--- src/utils/Hit.jsx
+++ src/utils/Hit.jsx
@@ -8,7 +8,7 @@ const Hit = ({ hit }) => {
         href={`/posts/${hit.objectID}`}
         className="hit-link block py-0 hover:bg-gray-300"
       >
-        <h3 className="text-md font-bold font-serif hover:text-yellow-600">{hit.title}</h3>
+        <h3 className="text-md font-bold font-serif mb-0 hover:text-yellow-600">{hit.title}</h3>
         <span className="text-sm text-indent-0 text-gray-600">
           {hit.category} || {hit.neighborhood}
         </span>
